caa42c50c5dfe8efab74d07a625b2aca13104eeb
Anjar Tiyo Saputro <93272408+AnjarTiyo@users.noreply.github.com>
2022-11-22 00:48:17 +0700

    Delete src/test/java/com/tiket/search directory

--- src/test/java/com/tiket/search/SearchForm.java
+++
@@ -1,8 +0,0 @@
-package com.tiket.search;
-
-import net.serenitybdd.screenplay.targets.Target;
-
-class SearchForm {
-    static Target SEARCH_FIELD = Target.the("search field").locatedBy("#searchInput");
-
-}
--- src/test/java/com/tiket/search/WikipediaArticle.java
+++
@@ -1,7 +0,0 @@
-package com.tiket.search;
-
-import net.serenitybdd.screenplay.targets.Target;
-
-public class WikipediaArticle {
-    public static final Target HEADING =  Target.the("article identifier").locatedBy("#firstHeading");
-}
