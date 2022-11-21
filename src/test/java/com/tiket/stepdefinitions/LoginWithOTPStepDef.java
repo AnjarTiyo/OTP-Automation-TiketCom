2bf950c1a25789e96634f98aa131e4e6689e8ba8
AnjarTiyo <anjar.jog@gmail.com>
2022-11-21 17:12:30 +0700

    block code for fetch OTP

--- src/test/java/com/tiket/stepdefinitions/LoginWithOTPStepDef.java
+++ src/test/java/com/tiket/stepdefinitions/LoginWithOTPStepDef.java
@@ -1,10 +1,15 @@
 package com.tiket.stepdefinitions;
 
 import com.tiket.actions.UserActions;
+import com.tiket.tasks.OTPHandlingWithJSoup;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
+
+import java.io.IOException;
 
 import static com.tiket.model.UserData.*;
 import static com.tiket.pages.LoginPage.*;
@@ -14,8 +19,6 @@ import static net.serenitybdd.core.Serenity.getDriver;
 public class LoginWithOTPStepDef extends UserActions {
 
     WebDriver driver = getDriver();
-
-    private String OTPNumber;
 
     @Given("User already on Login Page")
     public void alreadyOnLoginPage() {
@@ -29,8 +32,12 @@ public class LoginWithOTPStepDef extends UserActions {
     }
 
     @And("User input OTP")
-    public void userInputOTP() throws InterruptedException {
-        Thread.sleep(5000);
+    public void userInputOTP() throws InterruptedException, IOException {
+        Thread.sleep(5000); //handle sms sent delay
         inputTextTo(getOTPNumber(), OTP_FIELD);
+    }
+
+    @Then("User logged in")
+    public void userLoggedIn() {
     }
 }
--- src/test/java/com/tiket/tasks/OTPHandling.java
+++ src/test/java/com/tiket/tasks/OTPHandling.java
@@ -4,33 +4,51 @@ import com.twilio.Twilio;
 import com.twilio.base.ResourceSet;
 import com.twilio.rest.api.v2010.account.Message;
 
+import java.time.ZonedDateTime;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class OTPHandling {
 
     public static final String ACCOUNT_SID = "AC03194ad83714d956240116baef75ade4";
-    public static final String AUTH_TOKEN = "b8c52cd9f14dc34ae34122d393d1657a";
+    public static final String AUTH_TOKEN = "c7e366fe6160afc93725247a5f4df5fd";
 
     public static final String TIKET_PHONE_NUMBER = "+16238888859";
 
     public static String getOTPNumber(){
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-        String smsBody = getMessage();
-        System.out.println(smsBody);
-        String OTPNumber = smsBody.replaceAll("[^-?0-9]+", " ");
-        System.out.println(OTPNumber);
+//        String smsBody = getMessage();
+        String smsBody = getOTP2();
+//        System.out.println(smsBody);
+//        String OTPNumber = smsBody.replaceAll("[^-?0-9]{4}", " ");
+        String OTPNumber = smsBody.substring(102, 106);
+//        String OTPNumber = filter.replaceAll("[OTP: ]", "");
+//        System.out.println(OTPNumber);(^?<=OTP :)[0-9]{4}
         return OTPNumber;
     }
 
     public static String getMessage() {
         return getMessages().filter(m -> m.getDirection().compareTo(Message.Direction.INBOUND) == 0)
-                .filter(m -> m.getTo().equals("+13343734019")).map(Message::getBody).findFirst()
+                .filter(m -> m.getTo().equals(TIKET_PHONE_NUMBER)).map(Message::getBody).findFirst()
                 .orElseThrow(IllegalStateException::new);
     }
 
     private static Stream<Message> getMessages() {
         ResourceSet<Message> messages = Message.reader(ACCOUNT_SID).read();
         return StreamSupport.stream(messages.spliterator(), false);
+    }
+
+    public static String getOTP2(){
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        String msg = null;
+//         Untuk get semua mesasge
+        ResourceSet<Message> messages = Message.reader()
+                .setFrom(new com.twilio.type.PhoneNumber(TIKET_PHONE_NUMBER))
+                .limit(1)
+                .read();
+        for(Message record : messages){
+            msg = record.getBody();
+        }
+        return msg;
     }
 }
